83908de8ac71f9db4286f08a289168ec41d35bb7
Ben Anderson <benjammin412@gmail.com>
2020-04-11 12:48:46 -0500

    updated files

--- scrape_mars.py
+++ scrape_mars.py
@@ -133,7 +133,7 @@ def scrape():
         'news_p': news_p,
         'featured_image_url': featured_image_url,
         'mars_weather': mars_weather,
-        'html_table': html_table,
+        'html_table': table,
         'hemisphere_image_urls': hemisphere_image_urls
     }
 
